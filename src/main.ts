033f0855139b81a3de992491141c9ab06acbec33
Emmanuel Telewik <emmanuel.telewik@threetwentyfive.com.au>
2023-06-16 09:13:14 +0800

    move cors origin to env vars

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     allowedHeaders: ['content-type', 'Authorization'],
-    origin: 'http://localhost:4000',
-    credentials: true,
-  });
-  app.enableCors({
-    allowedHeaders: ['content-type', 'Authorization'],
-    origin: 'https://edlabs.netlify.app',
+    origin: process.env.ALLOWED_ORIGIN || 'http://localhost:4000',
     credentials: true,
   });
   app.use(cookieParser());
